589a27ef9e552fa9468b53547585ce1b35554ff4
Richard Nees <richard.nees@vimn.com>
2016-11-28 13:49:58 +0100

    Better layout

--- CollectionViewSyncBehaviour/DescriptionCollectionViewCell.swift
+++ CollectionViewSyncBehaviour/DescriptionCollectionViewCell.swift
@@ -7,14 +7,8 @@ class DestinationCollectionViewCell: UICollectionViewCell {
 
     var line: UBahnLine? {
         didSet {
-            layer.shadowOpacity = 0.5
-            layer.shadowOffset = CGSize(width: 2, height: 0)
-            layer.cornerRadius = 10.0
-            layer.borderWidth = 1.0
-            
             if let line = line {
-                layer.borderColor = line.textColor.cgColor
-                backgroundColor = line.color
+                backgroundColor = UIColor.clear
                 titleLabel.text = line.route.1
                 titleLabel.textColor = line.textColor
             }
--- CollectionViewSyncBehaviour/OriginCollectionViewCell.swift
+++ CollectionViewSyncBehaviour/OriginCollectionViewCell.swift
@@ -7,14 +7,8 @@ class OriginCollectionViewCell: UICollectionViewCell {
 
     var line: UBahnLine? {
         didSet {
-            layer.shadowOpacity = 0.5
-            layer.shadowOffset = CGSize(width: 2, height: 0)
-            layer.cornerRadius = 10.0
-            layer.borderWidth = 1.0
-            
             if let line = line {
-                layer.borderColor = line.textColor.cgColor
-                backgroundColor = line.color
+                backgroundColor = UIColor.clear
                 titleLabel.text = line.route.0
                 titleLabel.textColor = line.textColor
             }
